16715f36e7e5b82190d0bb238814ee1677c91377
Mahmoud Ismail <ismailm7373@gmail.com>
2024-08-15 03:48:40 +0300

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,1 @@
 # Using strace, find out why Apache is returning a 500 error. Once finding the issue, fixing it and then automating it using Puppet
-
-exec {'replaces wrong php filetype':
-  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
-  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
-  onlyif  => 'test -f /var/www/html/wp-settings.php'
-}
