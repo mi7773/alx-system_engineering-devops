e5441cd686cce7d07d104548e860ca07db6a2831
Mahmoud Ismail <ismailm7373@gmail.com>
2024-08-15 03:34:53 +0300

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,8 @@
 # Using strace, find out why Apache is returning a 500 error. Once finding the issue, fixing it and then automating it using Puppet
 
+# draft
 file { '/etc/draft':
   ensure  => file,
+  mode    => '0644'
   content => 'draft',
 }
