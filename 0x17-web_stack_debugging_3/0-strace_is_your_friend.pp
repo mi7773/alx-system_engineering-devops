d52ebe7e016e8e35cb37f7fb9ade1e8175d54369
Mahmoud Ismail <ismailm7373@gmail.com>
2024-08-15 04:44:16 +0300

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Using strace, find out why Apache is returning a 500 error. Once finding the issue, fixing it and then automating it using Puppet
 
 exec {'php':
-  command  => 'sed -i "s/pph/php/g" /var/site/test.php',
+  command  => 'sed -i "s/pph/php/g" /var/www/html/setting.php',
   provider => shell,
 }
