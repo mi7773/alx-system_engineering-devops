a9e7a2c0ec1a3bf7d93ef710b3c6c3a9f1d25e01
Mahmoud Ismail <ismailm7373@gmail.com>
2024-08-15 03:33:01 +0300

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
 # Using strace, find out why Apache is returning a 500 error. Once finding the issue, fixing it and then automating it using Puppet
 
-package { 'apache2':
-  ensure => installed,
-}
-
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => Package['apache2'],
+file { '/etc/draft':
+  ensure  => file,
+  content => 'draft',
 }
