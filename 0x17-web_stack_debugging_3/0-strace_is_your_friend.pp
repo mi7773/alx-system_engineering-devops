9b20f095ea728e0aef0f7c7d65e4bfddbbe4cfa8
Mahmoud Ismail <ismailm7373@gmail.com>
2024-08-15 05:05:33 +0300

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Using strace, find out why Apache is returning a 500 error. Once finding the issue, fixing it and then automating it using Puppet
 
-file { '/test':
-  ensure  => file,
-  content => 'test',
+exec { 'php':
+  command => 'sed -i s/phpo/php/g /var/www/html/wp-settings.php',
+  path    => '/bin',
 }
