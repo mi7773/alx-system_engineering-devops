d2d3caf38b900e6ce4dca12c4f388ce8918626c1
Mahmoud Ismail <ismailm7373@gmail.com>
2024-08-15 05:19:08 +0300

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Using strace, find out why Apache is returning a 500 error. Once finding the issue, fixing it and then automating it using Puppet
 
-file { '/test/':
+file { '/test':
   ensure  => file,
   content => 'test'
 }
