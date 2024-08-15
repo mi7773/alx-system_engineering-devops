15287a72cdfa04a1793cd75e4bc9e3ee116c492f
Mahmoud Ismail <ismailm7373@gmail.com>
2024-08-15 04:15:43 +0300

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Using strace, find out why Apache is returning a 500 error. Once finding the issue, fixing it and then automating it using Puppet
 
-exec { 'install_apache2':
+exec {'install_apache2':
   command  => 'apt install apache2',
   unless   => 'dpkg -l | grep apache2',
   provider => shell,
