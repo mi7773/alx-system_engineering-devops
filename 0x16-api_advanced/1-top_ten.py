dca156f714b0b96e66ab105da4db1272eb5b831b
Mahmoud Ismail <ismailm7373@gmail.com>
2024-08-16 08:31:58 +0300

    task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -17,7 +17,9 @@ def top_ten(subreddit):
     """
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': 'Custom'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         # d = json.dumps(data, indent=4)
