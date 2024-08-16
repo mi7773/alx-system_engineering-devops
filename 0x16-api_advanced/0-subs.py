c5613e039ce1173878ea9a1b638a6f132478d07a
Mahmoud Ismail <ismailm7373@gmail.com>
2024-08-16 07:23:16 +0300

    task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    queries Reddit API and returns the number of
+    subscribers (not active users, total subscribers)
+    for a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit.
+    Returns:
+        int: The number of subscribers.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'myapp/1.0 (by /u/myusername)'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data', {}).get('subscribers', 0)
+    else:
+        return 0
