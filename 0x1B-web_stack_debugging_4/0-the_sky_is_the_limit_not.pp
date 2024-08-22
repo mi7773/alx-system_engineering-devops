6c356bf348f79529e000febfc1c70ae9b6c5c894
Mahmoud Ismail <ismailm7373@gmail.com>
2024-08-22 07:15:32 +0300

    task

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # 0-the_sky_is_the_limit_not.pp
 
 exec { '0-the_sky_is_the_limit_not.pp':
-  command => '/bin/bash sed -i s/15/4096/g /etc/default/nginx; service nginx restart'
+  command => '/bin/sed -i s/15/4096/g /etc/default/nginx; service nginx restart'
 }
