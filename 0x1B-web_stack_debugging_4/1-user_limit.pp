ccc867dc67cb2b641208a127402c89d4d5906679
Mahmoud Ismail <ismailm7373@gmail.com>
2024-08-22 07:43:26 +0300

    task

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,6 @@
 # 1-user_limit.pp
 
 exec { '1-user_limit.pp':
-  command => '/bin/sed -i -e "s/hard nofile 5/hard nofile 2500" -e "s/soft nofile 4/soft nofile 2000" /etc/security/limits.conf',
+  command => '/bin/sed -i -e "s/hard nofile 5/hard nofile 2500" \
+  -e "s/soft nofile 4/soft nofile 2000" /etc/security/limits.conf',
 }
